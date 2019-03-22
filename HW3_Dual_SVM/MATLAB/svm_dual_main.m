4274c3225f17bc3ac039515084d0690bbdf868ab
Selma Wanna <slwanna@utexas.edu>
2019-03-22 16:44:38 -0500

    almost all testing for RBF complete. still need acc vs training data

--- HW3_Dual_SVM/MATLAB/svm_dual_main.m
+++ HW3_Dual_SVM/MATLAB/svm_dual_main.m
@@ -3,25 +3,25 @@
 max_iter = 1000;
 
 % create double for loop for C and gamma training
-g_size = 20;
-C_size = 10;
-gamma_vec = logspace(-5, 5, g_size);
-C_vec = logspace(-3, 5, C_size);
+% g_size = 20;
+% C_size = 10;
+% gamma_vec = logspace(-5, 5, g_size);
+% C_vec = logspace(-3, 5, C_size);
+% 
+% acc_mat = zeros(g_size, C_size);
 
-acc_mat = zeros(g_size, C_size);
-
-for i = 1:g_size
-    for j = 1:C_size
+% for i = 1:g_size
+%     for j = 1:C_size
         [support_vecs, conv, w, b] = svm_dual_train(train_img_col, ...
-            svm_labels, max_iter, gamma_vec(i), C_vec(j));
+            svm_labels, max_iter, 1.83298071, 0.001);
         % predictions
         y_hat = sign(mtimes(w',test_img_col) + b);
         % calc acc
         acc = calc_acc(y_hat', test_svm_labels);
-        acc_mat(i, j) = acc;
-        j
-        conv
-    end
-    i
-end
+%         acc_mat(i, j) = acc;
+%         j
+%         conv
+%     end
+%     i
+% end
 disp('program finished')
