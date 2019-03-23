357fc4c58876152ba9cda7c564ce09157e1a5596
Selma Wanna <slwanna@utexas.edu>
2019-03-22 22:17:54 -0500

    all rbf data collected (still need poly kernel implementation and PCA acc from assignment 1)

---
+++ HW3_Dual_SVM/MATLAB/acc_vs_training_plot.m
@@ -0,0 +1,4 @@
+scatter(perc, acc_vec)
+title('Percentage of Training Data vs Model Accuracy')
+xlabel('Percentage of Training Data')
+ylabel('Model Accuracy')
--- HW3_Dual_SVM/MATLAB/svm_dual_main.m
+++ HW3_Dual_SVM/MATLAB/svm_dual_main.m
@@ -1,4 +1,4 @@
-[train_img_col, svm_labels] = training_data_input('digits.mat');
+%[train_img_col, svm_labels] = training_data_input('digits.mat');
 [test_img_col, test_svm_labels] = test_data('digits.mat');
 max_iter = 1000;
 
@@ -10,14 +10,20 @@ max_iter = 1000;
 % 
 % acc_mat = zeros(g_size, C_size);
 
+perc = linspace(0.001, 0.2, 15);
+acc_vec = zeros(1, length(perc));
 % for i = 1:g_size
 %     for j = 1:C_size
+for i = 1:length(perc)
+        [train_img_col, svm_labels] = training_data_input('digits.mat', perc(i));
         [support_vecs, conv, w, b] = svm_dual_train(train_img_col, ...
-            svm_labels, max_iter, 1.83298071, 0.001);
+        svm_labels, max_iter, 1.83298071, 0.001);
         % predictions
         y_hat = sign(mtimes(w',test_img_col) + b);
         % calc acc
         acc = calc_acc(y_hat', test_svm_labels);
+        acc_vec(i) = acc;
+end
 %         acc_mat(i, j) = acc;
 %         j
 %         conv
