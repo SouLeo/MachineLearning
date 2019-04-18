fixed backprop code, holy shit. need to finish SGD and do testing still

HW4_DNN_MNIST/activationfuncs.py
@@ -1,11 +1,23 @@
 import numpy as np
 
 
-def sigmoid(z):
-    return 1.0/(1.0 + np.exp(-z))
+class Sigmoid(object):
 
+    @staticmethod
+    def fn(z):
+        ans = 1.0 / (1.0 + np.exp(-z))
+        return ans
 
-def sigmoid_deriv(z):
-    return sigmoid(z)*(1-sigmoid(z))
+    @staticmethod
+    def deriv(z):
+        return Sigmoid.fn(z)*(1-Sigmoid.fn(z))
 
-# TODO: Add tanh, ReLu, and ELU activation functions
+# def sigmoid(z):
+#     ans = 1.0/(1.0 + np.exp(-z))
+#     return ans
+#
+#
+# def sigmoid_deriv(z):
+#     return sigmoid(z)*(1-sigmoid(z))
+#
+# # TODO: Add tanh, ReLu, and ELU activation functions

HW4_DNN_MNIST/ffnetwork.py
@@ -1,11 +1,11 @@
 import numpy as np
 from inputData import mat_input
 import activationfuncs
-
+import cost_functions
 
 class FFNetwork(object):
 
-    def __init__(self, layers_and_dim):
+    def __init__(self, layers_and_dim, cost, activ):
         self.layers_and_dim = layers_and_dim
         self.num_layers = len(self.layers_and_dim)
         self.num_neurons = np.sum(self.layers_and_dim)
@@ -24,15 +24,16 @@ class FFNetwork(object):
         weight_combos = np.delete(weight_combos, 0, 0)
         self.weights = [np.random.randn(x, y) for x, y in weight_combos]
         # print(self.weights[0].shape)
-        #self.cost = cost
+        self.cost = cost
+        self.activ = activ
 
     def create_mini_batch(self, train_img, train_labels, mini_batch_size):
-        mini_batch_img = [train_img[:, x:x+mini_batch_size]
+        mini_batch_img = [train_img[:, x:x + mini_batch_size]
                           for x in range(0, train_img.shape[1], mini_batch_size)]
-        mini_batch_label = [train_labels[x:x+mini_batch_size]
+        mini_batch_label = [train_labels[x:x + mini_batch_size]
                             for x in range(0, len(train_labels), mini_batch_size)]
         # print(mini_batch_img[0].shape)
-        # print(len(mini_batch_label[0]))
+        # print(len(mini_batch_label))
         return mini_batch_img, mini_batch_label
 
     def stoch_grad_desc(self, epochs, learn_rate, mini_batch_img, mini_batch_labels):
@@ -57,6 +58,7 @@ class FFNetwork(object):
         # print(dL_dw[2].shape)
         for i in range(len(label)):
             self.backpropagation(img[:, i], label[i])
+        # self.backpropagation(img, label)
             # TODO: finish writing for loop
 
     def backpropagation(self, img, label):
@@ -65,22 +67,53 @@ class FFNetwork(object):
         dL_db = [np.zeros(b.shape) for b in self.biases]
         dL_dw = [np.zeros(w.shape) for w in self.weights]
         #
-        activation = img
-        activation_list = [img]
+
+        activation = np.expand_dims(img, axis=1)  #
+        # print(activation.shape)
+        activation_list = [activation]
         z_s = []  # preactivation func. vectors (list z's)
 
         # print(len(self.biases))
         # print(len(self.weights))
-        # print(self.weights[0].shape)
+        # print(self.weights[2].shape)
 
         for i in range(len(self.biases)):
-            # print(w.shape)
+            # print(self.weights[i].shape)
+            # print(self.biases[i].shape)
+            # print(activation.shape)
             z = np.dot(self.weights[i], activation) + self.biases[i]
+            # print(z.shape)
             z_s.append(z)
-            activation = activationfuncs.sigmoid(z)  #overflows
+            activation = self.activ.fn(z)  # overflows
             activation_list.append(activation)
+            # print(activation.shape)
+            # print(activation.max())
+            # print(activation.min())
+            # print(activation.shape)
 
+        #print(activation_list[2].shape)
         # backprop algo starts:
+        delta = self.cost.delta(activation_list[-1], label)
+
+        dL_db[-1] = delta
+        dL_dw[-1] = np.dot(delta, activation_list[-2].transpose())
+
+        # print(self.num_layers)
+        for i in range(2, self.num_layers):
+            z = z_s[-i]  # (16 by 16)
+            sp = self.activ.deriv(z)  # change to class, like cost_func
+            # watch line beneath
+            delta = np.dot(self.weights[-i + 1].transpose(), delta) * sp
+            #print(activation_list[-i - 1].shape)
+            dL_db[-i] = delta
+            # print(i)
+            # print(delta.shape)
+            # print(activation_list[-1].shape)
+            # print(activation_list[-2].shape)
+            # print(activation_list[-3].shape)
+            # print(activation_list[-4].shape)
+            dL_dw[-i] = np.dot(delta, activation_list[-i - 1].transpose())
+        return dL_dw, dL_db
 
 
 def main():
@@ -88,13 +121,16 @@ def main():
     epochs = 4
     learn_rate = 0.5
     mini_batch_size = 100
+    cost = cost_functions.CrossEntropyLoss
+    activ = activationfuncs.Sigmoid
     nn_architecture = np.array([784, 16, 16, 10])
 
-    test = FFNetwork(nn_architecture)
+    test = FFNetwork(nn_architecture, cost, activ)
     # TODO: Put following code into a ffnetwork member func.
     train_images, train_labels, test_images, test_labels = mat_input()
     mini_img, mini_lab = test.create_mini_batch(train_images, train_labels, mini_batch_size)
     test.stoch_grad_desc(epochs, learn_rate, mini_img, mini_lab)
+    print('Done!')
 
 
 if __name__ == '__main__':