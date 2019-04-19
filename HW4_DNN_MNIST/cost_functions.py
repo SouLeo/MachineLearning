1543130906b1730b38b20d4694dc511de04e7f18
Selma Wanna <slwanna@utexas.edu>
2019-04-18 21:38:42 -0500

    wrote hinge func, todo: test hinge

--- HW4_DNN_MNIST/cost_functions.py
+++ HW4_DNN_MNIST/cost_functions.py
@@ -11,14 +11,22 @@ class CrossEntropyLoss(object):
     def delta(a, y):
         return a-y
 
-# class HingeLoss(object):
-#
-#     @staticmethod
-#     def fn(truth_label, predicted):
-#         pos = np.sum(truth_label * predicted, axis=-1)
-#         neg = np.max((1-truth_label) * predicted, axis=-1)
-#         return np.mean(np.max(0.0, neg - pos + 1), axis=-1)
-#
-#     @staticmethod
-#     def delta():
-#         # blah
+
+class HingeLoss(object):
+
+    @staticmethod
+    def fn(active_func, z, y):
+        return np.max(0, 1 + active_func.fn(z) - y)
+
+    @staticmethod
+    def delta(active_func, z, a, y):
+        temp = active_func(z) - y
+        ans = np.zeros(temp.shape)
+        ans[temp >= -1] = active_func.deriv(z)*a
+        return ans
+
+#    @staticmethod
+#    def fn(truth_label, predicted):
+        # pos = np.sum(truth_label * predicted, axis=-1)
+        # neg = np.amax((1.0-truth_label) * predicted, axis=-1)
+        # return np.mean(np.max(0.0, neg - pos + 1), axis=-1)
